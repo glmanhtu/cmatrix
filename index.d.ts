81443979dd5e3fee16df6a06e4f80dad25d9363c
VU Manh Tu <glmanhtu@gmail.com>
2024-04-22 09:23:26 +0200

    Update

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,6 @@ declare module 'cmatrix' {
     export class JMatrix {
         constructor(rows: number, cols: number, initValue: number);
         public setCell(row: number, col: number, value: number): void;
-        public mds(dimensions: number, max_iter: number): number[][];
+        public mds(dimensions: number, max_iter: number, seed: number): number[][];
     }
 }
